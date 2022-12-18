d2232e5f54dfa27a691de723a8e61fd3ae36c254
Magomedcoder <info@magomedcoder.ru>
2022-12-18 18:19:03 +0300

    Added hosting window finder

--- Snake/Helpers/HostingWindowFinder.swift
+++ Snake/Helpers/HostingWindowFinder.swift
@@ -9,6 +9,21 @@ import SwiftUI
 
 extension View {
     func withHostingWindow(_ callback: @escaping (UIWindow?) -> Void) -> some View {
-        self.background()
+        self.background(HostingWindowFinder(callback: callback))
     }
+}
+
+struct HostingWindowFinder: UIViewRepresentable {
+    
+    var callback: (UIWindow?) -> ()
+
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView()
+        DispatchQueue.main.async { [weak view] in
+            self.callback(view?.window)
+        }
+        return view
+    }
+
+    func updateUIView(_ uiView: UIView, context: Context) {}
 }
